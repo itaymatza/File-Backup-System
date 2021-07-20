fbc6fcf636257036802f869e92e2566dadbf5615
Itay Matza <itaymatza.outlook.com>
2021-07-20 20:21:30 +0300

    Add user menu

--- Client/client_helper.py
+++ Client/client_helper.py
@@ -1,18 +1,19 @@
-""" Server helper - functions related to server management"""
+""" Client helper - functions related to client management"""
 
 
-# Reads port number from ./port.info file.
-def get_tcp_port(file):
+# Reads server's ip address and port number from file.
+def get_server_ip_and_port(server_info_file):
     try:
-        with open(file) as port_file:
-            data = port_file.read().strip().split()
-            if len(data) > 1:
-                raise Exception('Error: port.info file should contain just port number.')
-            port_info = int(data[0])
-            if not 0 < port_info < 65536:
-                raise ValueError
+        with open(server_info_file) as server:
+            if len(server.readlines()) > 1:
+                print('Error: ' + server_info_file + ' file should be in format "server:port".')
+                exit(-1)
+            server.seek(0)
+            read_data = server.read()
+            ip_info, port_info = read_data.strip().split(':')
+            if len(port_info.split()) != 1 or not 0 < int(port_info) < 65536:
+                print('Error: Invalid port number.')
     except IOError:
-        raise IOError("Error: port.info file is not accessible.")
-    except ValueError:
-        raise ValueError("Error: Invalid port number.") from None
-    return port_info
+        print('Error: ' + server_info_file + ' file is not accessible.')
+        exit(-1)
+    return ip_info, int(port_info)
